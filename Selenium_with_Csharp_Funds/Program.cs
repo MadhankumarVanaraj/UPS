9e5e21446b24bde83c145f8e1a33bc9ec431867c
MadhankumarVanaraj <kumar.madhanmca@gmail.com>
2026-01-21 16:47:57 +0530

    Changed Program Name Hands-on done on variables.

---
+++ Selenium_with_Csharp_Funds/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Selenium_with_Csharp_Funds
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 10;
+            Console.WriteLine("The value of a is "+ a);
+
+            string associateName="Madhan Kumar Vanaraj";
+
+            Console.WriteLine("The name of the Associate is " + associateName);
+            Console.WriteLine($"The name of the associate is {associateName}");
+
+            var age = 23;
+            Console.WriteLine($"The age of the Assocaite is {age}");
+
+            dynamic height = "5.3";
+            Console.WriteLine($"The height of the Associate is {height}");
+
+            height = "Hello";
+
+            Console.WriteLine($"The height is {height}");
+		}
+    }
+}
